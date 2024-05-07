353add35464168cc316b829c2502cb757e009747
mackliu <macklun@gmail.com>
2024-05-07 09:48:29 +0800

    使用session來儲存登入狀態

--- session/check.php
+++ session/check.php
@@ -1,14 +1,16 @@
 <?php
+session_start();
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
 if($acc=='admin'  && $pw=='1234' ){
 //如果正確
-    header('location:result.php?acc='.$acc);
+    $_SESSION['login']=$acc;
+    header('location:result.php');
 }else{
 //如果有一不正確
-
-    header("location:error.php?acc=$acc");
+    $_SESSION['error']='帳號或密碼錯誤';
+    header("location:login.php");
 }
 
 
