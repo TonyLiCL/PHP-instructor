22bb11142f0cfa28edd080314019372440739958
mackliu <macklun@gmail.com>
2024-05-07 11:23:35 +0800

    使用cookie來完成會員登入功能

--- cookie/check.php
+++ cookie/check.php
@@ -1,14 +1,16 @@
 <?php
+date_default_timezone_set("Asia/Taipei");
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
 if($acc=='admin'  && $pw=='1234' ){
 //如果正確
-    header('location:result.php?acc='.$acc);
+    setcookie('login',$acc,time()+60);
+    header('location:result.php');
 }else{
 //如果有一不正確
-
-    header("location:error.php?acc=$acc");
+    setcookie('error','帳號或密碼錯誤',time()+60);
+    header("location:login.php");
 }
 
 
